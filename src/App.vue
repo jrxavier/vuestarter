5047ad512cb3053d5aed146979dbac255021895a
Jose Xavier <jrxavier@gmail.com>
2019-03-30 08:53:57 -0300

    Stylling the search bar

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div class="container">
         <SearchBar @termChange="onTermChange"></SearchBar>
         <!-- 
         v-bind pode ser substituido por :
--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -15,6 +15,14 @@ export default {
 }
 </script>
 
-<style>
+<!-- a definição SCOPE garante que o CSS só se aplica ao componente em questão -->
+<style scoped>
+    input {
+        width: 75%;
+    }
 
+    div {
+        text-align: center;
+        margin: 20px;
+    }
 </style>
